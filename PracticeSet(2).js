a5657521bc23c3847910fad254ce44b58fd8a12c
SinghBanta <singhbanta84@gmail.com>
2025-02-17 16:11:45 +0530

    add practice 2

---
+++ PracticeSet(2).js
@@ -0,0 +1,50 @@
+//1. Use Logical operator to find whether the age of person lies age between 10 and 20.
+
+// let age=12;
+// console.log(age>10 && age<20)
+//true
+
+
+//2.Switch statements
+
+// let day="Monday";
+// switch(day){
+//     case ("Monday"):
+//         console.log("Today is Monday");
+//         break;
+//     case ("Tuesday"):
+//         console.log("Today is Tuesday");
+//         break;
+//     case ("Wednesday"):
+//         console.log("Today is Wednesday");
+//         break;    
+//     default:
+//         console.log("Today is not a weekday");
+//         break;
+// }
+
+
+//3.Write a JavaScript programme to find whether a number is divisible by 2 and 3.
+
+// let number=6;
+// if(number%2==0 && number%3==0){
+//     console.log("The number is divisible by 2 and 3");
+// }else{
+//     console.log("The number is not divisible by 2 and 3");
+// }
+
+
+//4.Write a JavaScript programme to find whether a number is divisible by 2 or 3.
+
+// let number=9;
+// if(number%2==0 || number%3==0){
+//     console.log("The number is divisible by either 2 or 3");
+// }else{
+//     console.log("The number is not divisible by either 2 or 3");
+// }
+
+
+//5.Print you can drive or you can't using ternary operator.
+
+// let age=17;
+// console.log(age>=18? "You can drive":"You can't drive")
